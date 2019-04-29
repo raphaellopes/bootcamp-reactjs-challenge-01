e79f00e8e5736a3faed78ca2213aa5bb87fdb600
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-29 17:30:05 -0300

    Add basic react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert('test update');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <p>It works!</p>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
