ed20a8b8421683c48aab1b1052b67f4880f426f1
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-29 17:40:23 -0300

    Add babel-eslint on eslintrc and create base posts object state on app component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 class App extends Component {
+  state = {
+    posts: [{
+      author: {
+        name: 'Rasta',
+        avatar: 'image',
+      },
+      date: '2019-04-29',
+      text: 'asdasd ad asdas das das dasd asd asd a',
+    }],
+  }
+
   render() {
+    const { posts } = this.state;
     return (
-      <p>It works!</p>
+      <p>{posts[0].text}</p>
     );
   }
 }
